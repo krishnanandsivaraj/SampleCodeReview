95ec1a3135408d4a6f9b830474c9eb826a38b458
Krishnanand Sivaraj <Krishnanand Sivaraj>
2017-01-17 09:49:15 +0530

    added xml summary comments and target any cpu

--- src/AddressProcessor/CSV/CSVReaderWriter.cs
+++ src/AddressProcessor/CSV/CSVReaderWriter.cs
@@ -11,11 +11,17 @@ namespace AddressProcessing.CSV
 
     public class CSVReaderWriter : ICSVReaderWriter, IDisposable
     {
+        /// <summary>  
+        ///  removed unnecessary readerstream as it is used once  
+        /// </summary> 
         private StreamWriter _writerStream = null;
 
         [Flags]
         public enum Mode { Read = 1, Create = 2 };
 
+        /// <summary>  
+        ///  converted multiple if else to switch
+        /// </summary>
         public bool Open(string fileName, Mode mode)
         {
             try
@@ -25,7 +31,10 @@ namespace AddressProcessing.CSV
                 case Mode.Read:
                     File.OpenText(fileName);
                     return true;
-                case Mode.Create:
+                    /// <summary>  
+                    ///  FileInfo not required as the streamreader handles internally
+                    /// </summary>
+                    case Mode.Create:
                     using(_writerStream=new StreamWriter(fileName,true)) { 
                         _writerStream.WriteLine();
                     }
@@ -34,8 +43,14 @@ namespace AddressProcessing.CSV
                         return false;
             }
             }
-            catch(ApplicationException ex)
+            /// <summary>  
+            ///  catching exceptions in try catch and not in else condition
+            /// </summary>
+            catch (ApplicationException ex)
             {
+                /// <summary>  
+                ///  used string interpolation c# 7.0
+                /// </summary>
                 throw new Exception($"Unknown file mode for {fileName} {ex.Message}");
             }
         }
@@ -45,23 +60,42 @@ namespace AddressProcessing.CSV
             using (_writerStream = new StreamWriter(fileName, true))
             {
                 var i = 0;
+                /// <summary>  
+                ///  used stringbuilder instead of string
+                /// </summary>
                 StringBuilder sb = new StringBuilder();
+                /// <summary>  
+                ///  replaced for with foreach
+                /// </summary>
                 foreach (string column in columns)
                 {
                     sb.Append(column);
+                    /// <summary>  
+                    ///  removed a variable to declare '\t'
+                    /// </summary>
                     if (i++ < columns.Length - 1) { sb.Append("\t"); }
                 }
                 _writerStream.WriteLine(sb);
             }
         }
 
+        /// <summary>  
+        ///  Read method without 'out' parameter removed.
+        /// </summary>
+
         public bool Read(out string column1, out string column2,string path)
         {
+         /// <summary>  
+         ///  using keyword to open and close a connection
+         /// </summary>
             using (var reader = new StreamReader(path))
             { 
             const int FIRST_COLUMN = 0, SECOND_COLUMN = 1;
             string line=reader.ReadLine();
-            if (string.IsNullOrEmpty(line)||string.IsNullOrWhiteSpace(line))
+                /// <summary>  
+                ///  if condition to check null is replaced with native string implementation.
+                /// </summary>
+                if (string.IsNullOrEmpty(line)||string.IsNullOrWhiteSpace(line))
             { column1 = column2 = null; return false; }
             else
             {
@@ -72,7 +106,9 @@ namespace AddressProcessing.CSV
             }
             };
         }
-
+        /// <summary>  
+        ///  removed the close method and IDisposable implemented.  Helps while creating objects in unit testing layer.
+        /// </summary>
         public void Dispose()
         {
             if (_writerStream != null)
